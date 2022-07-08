0e08b7298d3145b417d0bec8cb5d58fe960fd578
Alberto Vecina Sánchez <albertovecina@gmail.com>
2022-07-08 11:53:18 +0200

    Initial commit

---
+++ sample/src/main/java/com/vsa/drill/sample/SampleActivity.kt
@@ -0,0 +1,11 @@
+package com.vsa.drill.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
